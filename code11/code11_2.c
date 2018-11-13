d3580ae5dfaf71c5287f45e3ea67eaffe153dcf1
Rishabh <rishabhjain7b@gmail.com>
2018-11-13 19:47:47 +0530

    test1

---
+++ code11/code11_2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+  int x = 032;
+  printf("%d", x);
+  return 0;
+}
+
---
+++ code11/code11_3.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+ 
+int * gPtr;
+ 
+int main()
+{
+ int * lPtr = NULL;
+ 
+ if(gPtr == lPtr)
+ {
+   printf("Equal!");
+ }
+ else
+ {
+  printf("Not Equal");
+ }
+ 
+ return 0;
+}
+
