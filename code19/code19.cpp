02191bc85f1eecf273b8fa1a1bd49a8982a7342a
Rishabh <rishabhjain7b@gmail.com>
2018-11-21 00:23:42 +0530

    Character arithmetic

---
+++ code19/code19.cpp
@@ -0,0 +1,14 @@
+// A C++ program to demonstrate character 
+// arithmetic in C++. 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main() 
+{ 
+	char ch = 65; 
+	cout << ch << endl; 
+	cout << ch + 0 << endl; 
+	cout << char(ch + 32) << endl; 
+	return 0; 
+} 
+
