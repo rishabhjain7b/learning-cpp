f14c95cd1d8faedab9bf8fb13689305a68466240
Rishabh <rishabhjain7b@gmail.com>
2019-01-06 00:01:40 +0530

    print %

---
+++ code32/code32.c
@@ -0,0 +1,14 @@
+/* Program to print %*/
+#include<stdio.h> 
+/* Program to print %*/
+int main() 
+{ 
+	printf("%% \n");
+        printf("%c \n", '%');	
+	printf("%s \n", "%");
+	printf("\% ");
+	printf("\n");
+
+	return 0; 
+} 
+
